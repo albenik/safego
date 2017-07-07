04ac920b22262a9c33ea4eb6c6740ba40c41f8de
Veniamin Albaev <albenik@gmail.com>
2017-07-07 09:56:57 +0300

    New Goe() func which is handling passed function error result

--- recover.go
+++ recover.go
@@ -1,8 +1,9 @@
 package safego
 
 import (
-	"github.com/sirupsen/logrus"
 	"runtime/debug"
+
+	"github.com/sirupsen/logrus"
 )
 
 type RecoverFunc func(r interface{}, stack string)
@@ -20,14 +21,32 @@ func Go(fn func(), rfn RecoverFunc) {
 
 func Gol(label string, fn func(), log logrus.FieldLogger) {
 	go func() {
-		log.Debugf("go(%s): begin", label)
+		log.Debugf("go(%s) begin", label)
 		defer func() {
 			if r := recover(); r == nil {
-				log.Debugf("go(%s): end", label)
+				log.Debugf("go(%s) end", label)
 			} else {
-				log.Errorf("go(%s) error: %v\n%v", label, r, string(debug.Stack()))
+				log.Errorf("go(%s) panic %v\n%v", label, r, string(debug.Stack()))
 			}
 		}()
 		fn()
+	}()
+}
+
+func Goe(label string, fn func() error, log logrus.FieldLogger) {
+	go func() {
+		log.Debugf("go(%s) begin", label)
+		defer func() {
+			if r := recover(); r == nil {
+				log.Debugf("go(%s) end", label)
+			} else {
+				log.Errorf("go(%s) panic %v\n%v", label, r, string(debug.Stack()))
+			}
+		}()
+		if err := fn(); err != nil {
+			log.WithError(err).Errorf("go(%s) func error", label)
+		} else {
+			log.Debugf("go(%s) func success", label)
+		}
 	}()
 }
